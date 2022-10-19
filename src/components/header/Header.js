87244cf49ffdddab52ec96fd1ec73712f4afe742
Andres 'SHK' F. P <andrs.fp@gmail.com>
2022-10-19 15:56:20 +0200

    implements header input routing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,23 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import {HeaderDiv, SpacerDiv, HeaderInput} from "../../style/Style";
 
 export function Header(props) {
+    const navigate = useNavigate();
+    const onCollection = useLocation().pathname.slice(0, '/collection'.length) === '/collection';
+    const handleSubmit = (ev) => {
+        if (ev.key === "Enter") {
+            if(onCollection) {
+                navigate("/collection/search/" + ev.target.value);
+            } else {
+                navigate("/search/" + ev.target.value);
+            }
+        }
+    };
     return (
         <SpacerDiv>
             <HeaderDiv>
                 <p>Image Hunter</p>
-                <HeaderInput type="text" placeholder="Search photos of..."/>
+                <HeaderInput type="text" placeholder={onCollection ? "Filter by description..." : "Search photos with..."} onKeyDown={handleSubmit}/>
             </HeaderDiv>
         </SpacerDiv>
     );
